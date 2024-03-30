a6c6be8e1ff3a71ca4d3ff50990814dd303e9a17
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-03-30 14:38:49 +0530

    feat:

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-red-500">
+    <h1 class="w-full h-lvh flex justify-center items-center font-bold text-5xl text-green-500 font-mono">
       Hello world
     </h1>
   </div>
