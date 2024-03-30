6a5a3d6c6e8488bdea078b520c3d575f97bd6a5e
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-03-30 11:06:21 +0530

    feat:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,25 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
+    browser: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    "prettier/prettier": "error",
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
 };
